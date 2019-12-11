8dca08205e8ac7a9d31ed9b9e64622fe34c38d9a
steven-terrana <steventerrana@gmail.com>
2019-12-11 10:52:17 -0500

    Update node.groovy

--- node-labels/helper/node.groovy
+++ node-labels/helper/node.groovy
@@ -1,4 +1,6 @@
 void call(String label = null, Closure body){
+   echo "inside custom node block"
+   echo "going to use label: ${label ?: config.label ?: ""}"
    steps.node( label ?: config.label ?: "" ){
     body()
   }
